2ff221171072ece97379579d77a75daacfa7fcbf
luciengaitskell <luciengaitskell@users.noreply.github.com>
2016-01-14 15:33:15 +0000

    started new Problem_3

--- answers/Problem_3/Problem_3.py
+++ answers/Problem_3/Problem_3.py
@@ -1,6 +1,29 @@
 num=600851475143
+#num=10
 ii=num-1
+primes = []
 
+# primes
+while ii > 1:
+    jj=ii-1
+    flag=False
+    while jj>1:
+        flag=True
+        if ii % jj == 0:
+            flag=False
+            break
+        jj= jj-1
+    if flag == True:
+        primes.append(ii)
+        print primes
+    ii= ii-1
+
+print primes
+
+
+# OTHER
+
+'''
 while ii > 1:
     if num % ii == 0:
         jj=ii
@@ -18,3 +41,4 @@ while ii > 1:
     #print(ii)
 
 print ii
+'''
